b56c83d7ff6b793631f57a0d60bab01e4132af25
Niraj Vora <nsvora@ucdavis.edu>
2015-11-22 12:08:35 -0800

    modifies planets to rotate properly and adds Mars

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ void setup( ){
   size(400,400);
 }
 
-background(0);
+var inc = 0;
+var minc = 0;
+var Minc = 5;
 var Star = function( x, y, radius ) {
     this.x = x;
     this.y = y;
@@ -10,19 +12,41 @@ var Star = function( x, y, radius ) {
     rectMode(CENTER);
     rect(x, y, 2 * radius, 2 * radius, radius);
 };
-
-var Planet = function( dist, radius, ref, r, g, b) {
-    var r = r || 84;
-    var g = g || 98;
-    var b = b || 255;
-    
-    this.x = ref.x;
-    this.y = ref.y + dist;
-    
+var Planet = function( dist, radius, ref, speed, r, g, b) {
+    var r = r || 84, g = g || 98, b = b || 255;
+    var speed = speed || 0.1;
     fill(r, g, b);
     rectMode(CENTER);
+    this.x = (dist * cos(inc)) + ref.x;
+    this.y = (dist * sin(inc)) + ref.y;
     rect(this.x, this.y, 2 * radius, 2 * radius, radius);
+    inc += speed;
+};
+var Moon = function( dist, radius, ref, speed, r, g, b) {
+    var r = r || 84, g = g || 98, b = b || 255;
+    var speed = speed || 0.05;
+    fill(r, g, b);
+    rectMode(CENTER);
+    this.x = (dist * cos(minc)) + ref.x;
+    this.y = (dist * sin(minc)) + ref.y;
+    rect(this.x, this.y, 2 * radius, 2 * radius, radius);
+    minc += speed;
+};
+var Mars = function( dist, radius, ref, speed, r, g, b) {
+    var r = r || 84, g = g || 98, b = b || 255;
+    var speed = speed || 0.05;
+    fill(r, g, b);
+    rectMode(CENTER);
+    this.x = (dist * cos(Minc)) + ref.x;
+    this.y = (dist * sin(Minc)) + ref.y;
+    rect(this.x, this.y, 2 * radius, 2 * radius, radius);
+    Minc += speed;
 };
 
-var sun = new Star(200,200,10);
-var earth = new Planet(100, 4, sun);
+draw = function() {
+    background(0);
+    var sun = new Star(200,200,10);
+    var earth = new Planet(90, 5, sun);
+    var moon = new Moon(10, 3, earth, 0.5, 180,180,180);
+    var mars = new Mars(150, 6, sun, 0.2, 255, 0, 100);
+};
